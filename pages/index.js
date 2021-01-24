499ad9f61a6c104fcc8794acc20ada8fe2b80bb0
Nehemiah Kamolu <kamolunehemiah@gmail.com>
2021-01-24 22:30:17 +0300

    Mobile navigation bar and humberger icon, refactored

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,39 @@
 import Head from 'next/head'
+import { useState } from 'react'
 import styles from '../styles/Home.module.scss'
 
 export default function Home() {
+
+  const init_ul_sm_styles = {
+    transform:'translateY(-300px)',
+    transition: 'opacity 1s',
+    opacity:'0'
+  }
+
+  const [getUlSmStyles,setUlSmStyles] = useState(init_ul_sm_styles);
+  
+  const [isUlSmVisible,setUlSmVisibilty] = useState(false);
+ 
+  const onHumbergerIconClickedHandler = event =>{
+    event.preventDefault();
+
+    setUlSmVisibilty(!isUlSmVisible)
+    
+    const show_ul_sm_styles = {
+      transform:'translateY(20px)',
+      transition: 'opacity 1s',
+      opacity:'1'
+    }
+
+    const hide_ul_sm_styles = {
+      transform:'translateY(-300px)',
+      transition: 'opacity 1s',
+      opacity:'0'
+    }
+
+    setUlSmStyles(getUlSmStyles.transform === 'translateY(-300px)'? show_ul_sm_styles : hide_ul_sm_styles)
+
+  }
   return (
     <>
       <Head>
@@ -29,6 +61,42 @@ export default function Home() {
                   <li><a>Clients</a></li>
                   <li><a>Contact</a></li>
                 </ul>
+            </nav>
+
+            <nav className={styles.navbar_sm}>
+
+               <div className={styles.group}>
+                  <div className={styles.title}>
+                    <img src="" alt='alt logo'/>
+                    <a>KAMOKEN</a>
+                  </div>
+
+                  <div
+                      className={styles.humberger_icon}
+                      onClick={onHumbergerIconClickedHandler}
+                       >
+                        {!isUlSmVisible?
+                        <>
+                    <div className={styles.first_line}></div>
+                    <div className={styles.second_line}></div>
+                    <div className={styles.third_line}></div>
+                    </>:
+                    <div className={styles.close}>x</div>
+                    }
+                  </div>
+               </div>
+
+               <ul 
+                  className={styles.ul_sm} 
+                  style={getUlSmStyles}
+                  >
+                  <li><a>Home</a></li>
+                  <li><a>About</a></li>
+                  <li><a>Services</a></li>
+                  <li><a>Clients</a></li>
+                  <li><a>Contact</a></li>
+                </ul>
+
             </nav>
 
             <div className={styles.intro_text}>
